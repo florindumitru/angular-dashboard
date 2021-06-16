271ca79e1b8ea17d8a49b3da0d819fae210a76a1
Cătălin Dumitru <dumitru.catalin96@yahoo.com>
2021-06-16 21:54:57 +0300

    initial commit
    initial commit with base structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './core/login/login.component';
+import { SidenavComponent } from './core/sidenav/sidenav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '', redirectTo: 'login', pathMatch: 'full'
+  // },
+  {
+    path: '', redirectTo: 'main', pathMatch: 'full'
+  },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'main', component: SidenavComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { CoreMaterialModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './core/sidenav/sidenav.component';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CoreMaterialModule
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
