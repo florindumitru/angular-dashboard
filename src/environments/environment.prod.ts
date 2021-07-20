3c7a11f9c036c46df3717cc801da583b682ee2cd
Cătălin Dumitru <dumitru.catalin96@yahoo.com>
2021-07-20 22:19:59 +0300

    change output dir to display on github pages
    change output dir to display on github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDoFqx2pn8GmMz4VwbGOiwfwIdURHwHR1E",
+    authDomain: "angular-dashboard.github.io",
+    projectId: "ipds-app",
+    storageBucket: "ipds-app.appspot.com",
+    messagingSenderId: "805505372614",
+    appId: "1:805505372614:web:ff753fcd9e7c24f4a6e2c7",
+    measurementId: "G-0K6KCSZMS5"
+  }
 };
